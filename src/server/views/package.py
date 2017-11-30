a4413e294e37f8d683499a82aed27a9eb6f30066
Carolina Fernandez <carolina.fernandez@i2cat.net>
2017-11-30 19:03:56 +0100

    SHIELD-19: proxy onboard request to NFVO   - Allow remotely stored packages   - Introduce exception handling

--- src/server/views/package.py
+++ src/server/views/package.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import abort
 from flask import Blueprint
 from flask import jsonify
 from flask import request
+from flask import Response
 from nfv import package as pkg
 from server import content
 from server import endpoints
@@ -17,12 +17,23 @@ nfvo_views = Blueprint("nfvo_pkg_views", __name__)
 @content.expect_json_content
 def onboard_package():
     try:
-        if "application/json" not in request.headers.get("Content-Type", ""):
-            abort(400)
-        if content.data_not_in_request(request, ["path"]):
-            abort(418)
-        payload = pkg.onboard_package(request.json.get("path"))
-        output = payload
+        file_path = None
+        data = request.files
+        if data is not None:
+            pkg_bin = data.get("package")
+        output = pkg.onboard_package(pkg_bin)
+        return jsonify(output)
+    except Exception as e:
+        return "Error: %s" % str(e)
+
+@nfvo_views.route(endpoints.PKG_ONBOARD_REMOTE, methods=["POST"])
+@content.expect_json_content
+def onboard_package_remote():
+    try:
+        file_path = None
+        if content.data_in_request(request, ["path"]):
+            file_path = request.json.get("path")
+        output = pkg.onboard_package_remote(file_path)
         return jsonify(output)
     except Exception as e:
         return "Error: %s" % str(e)
@@ -32,3 +43,4 @@ def remove_package(vnsf_name):
     payload = pkg.remove_package(vnsf_name)
     output = payload
     return jsonify(output)
+
