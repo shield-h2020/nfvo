2b709c1a112152ca51b270a8bc766b0ebd90e8e5
Alfonso Egio <alfonso.egio@i2cat.net>
2018-03-21 16:09:01 +0100

    SHIELD-22: Create user in mongodb container when service starts

--- bin/docker/setup-nfvo.sh
+++ bin/docker/setup-nfvo.sh
@@ -4,6 +4,17 @@ REQ_PATH=${DST_PATH}/bin/deps/
 
 # APT requirements
 cat ${REQ_PATH}/nfvo-apt.txt | xargs apt-get install -y
+apt-get -y install netcat
+
+echo "Waiting for mongodb"
+until nc -z ${CNTR_DB} ${DB_PORT}
+do
+    echo "."
+    sleep 1
+done
+
+# Create database user in mongodb container
+mongo --host ${CNTR_DB}:${DB_PORT} --eval "db.runCommand({ createUser: \"${DB_USERNAME}\", pwd: \"${DB_PASSWORD}\", roles: [ {role: \"readWrite\", db: \"${DB_DBNAME}\"} ]})"
 
 # Pip requirements
 pip3 install -r ${REQ_PATH}/nfvo-pip.txt
