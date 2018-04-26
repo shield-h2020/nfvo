64137dc5aab15c7ee47c40a8d62682217c3702ba
Egio, Alfonso <alfonso.egio@i2cat.net>
2018-04-26 17:26:59 +0200

    SHIELD-22: Tabs/whitespaces fix

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 parse_options() {
-
     parse_cmd=`getopt -n$0 -o h:: -a --long test,teardown -- "$@"`
     if [ $? != 0 ] ; then
-	usage
-	echo
-	echo
-	exit 1
+        usage
+        echo
+        echo
+        exit 1
     fi
 
     eval set -- "$parse_cmd"
@@ -16,75 +15,70 @@ parse_options() {
 
     while [ $# -gt 0 ]
     do
-	case "$1" in
+        case "$1" in
 
-	    --test)
-		p_test=true
-		;;
+            --test)
+                p_test=true
+                ;;
 
-	    --teardown)
-		p_teardown=true
-		;;
+            --teardown)
+                p_teardown=true
+                ;;
 
-	    -h)
-		usage
-		exit 1
-		;;
+            -h)
+                usage
+                exit 1
+                ;;
 
-	    # End marker from getopt
-	     --)
-		shift
-		break
-		;;
+            # End marker from getopt
+             --)
+                shift
+                break
+                ;;
 
-	    -*)
-		echo "Unknown option $1"
-		usage
-		exit 1
-		;;
+            -*)
+                echo "Unknown option $1"
+                usage
+                exit 1
+                ;;
 
-	    *)
-		# Any additional parameter is an error.
-		echo "Too many parameters provided."
-		Usage
-		exit 1
-		;;
+            *)
+                # Any additional parameter is an error.
+                echo "Too many parameters provided."
+                Usage
+                exit 1
+                ;;
 
-	esac
-	shift
+        esac
+        shift
     done
 
     return $OPTIND
 }
 
 copy_conf() {
-
     # copy configuration sample files
     if [ ! -f ./conf/api.conf ]; then
-	cp conf/api.conf.sample conf/api.conf
+        cp conf/api.conf.sample conf/api.conf
     fi
 
     if [ ! -f ./conf/db.conf ]; then
-	cp conf/db.conf.sample conf/db.conf
+        cp conf/db.conf.sample conf/db.conf
     fi
 
     if [ ! -f ./conf/nfvo.conf ]; then
-	cp conf/nfvo.conf.sample conf/nfvo.conf
+        cp conf/nfvo.conf.sample conf/nfvo.conf
     fi
-
 }
 
 generate_certs() {
-
     # Generate certs and keys
     cd bin/
     ./gen_creds.sh
     cd ..
-
 }
 
 setup() {
-
     # Prepare host virtualenv (docker-compose)
     rm -rf venv
     virtualenv venv -p python3
@@ -92,20 +86,17 @@ setup() {
     pip install --upgrade pip setuptools
     pip install docker-compose jinja2
 
-    # # Render docker configuration
+    # Render docker configuration
     python docker/configure.py
 
-    # # Start docker compose
+    # Start docker compose
     docker-compose -f docker/docker-compose.yml up -d --force-recreate
-
 }
 
 cleanup() {
-
-    # # Cleanup
+    # Cleanup
     rm docker/docker-compose.yml
     rm docker/mongo-entrypoint/adduser.sh
-
 }
 
 parse_options "$@"
